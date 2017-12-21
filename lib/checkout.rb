1edcb4838471f0f256e4d63c5c35b214d6eb8a9f
Juan Camilo Hernández <camilo9020@gmail.com>
2017-12-21 22:38:38 +0100

    Add 3 methods to checkout to improve interaction with CLI:
    - show_discount: To show total order discount
    - list_products: To list products available in the store
    - list_order_items: To list order_items addend to the order

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -3,19 +3,19 @@ require_relative 'order'
 require_relative 'rules/rules'
 
 class Checkout
-  attr_reader :order, :total, :store
+  attr_reader :order, :store
 
   def initialize(pricing_rules)
     @rules = pricing_rules
     @store = Store.new
     @order = Order.new
-    @total = 0.00
   end
 
   def scan(code)
     product = @store.products[code]
     if product
       @order.add_item(product)
+      total
       "Product scanned"
     else
       "Product doesn't exist, try another code"
@@ -23,10 +23,31 @@ class Checkout
   end
 
   def total
+    @total = 0.00
+    @discount = 0.00
     @rules.apply_rules(@order.items)
     @order.items.each do |_key, item|
       @total += (item.unit_price * item.quantity - item.discount).round(2)
+      @discount += item.discount
     end
     @total
+  end
+
+  def show_discount
+    total
+    puts "got a discount of #{@discount}€ then " if @discount.positive?
+  end
+
+  def list_products
+    store.products.each { |k, v| puts "Product: #{v.name}  code: #{k} price: #{v.price}€" }
+  end
+
+  def list_order_items
+    if order.items.length.positive?
+      puts "You have scanned:"
+      order.items.each { |k, v| puts "#{v.quantity} #{k} units each for #{v.unit_price}€" }
+    else
+      puts "You don't have scanned products"
+    end
   end
 end
